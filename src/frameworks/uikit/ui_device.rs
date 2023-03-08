15e62c268f3559608e64a1cc330161d65c3a5473
upintheairsheep <43690204+upintheairsheep@users.noreply.github.com>
2023-03-07 19:21:12 -0800

    Update ui_device.rs

--- src/frameworks/uikit/ui_device.rs
+++ src/frameworks/uikit/ui_device.rs
@@ -52,6 +52,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     ns_string::get_static_str(env, "2.0")
 }
 
+- (id)systemName {
+    ns_string::get_static_str(env, "iPhoneOS")
+}
+
 - (bool)isMultitaskingSupported {
     false
 }
