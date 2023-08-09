22943f89a8741082ccef17a12e81cbabc6d9aec5
ciciplusplus <ciciplusplus@gmail.com>
2023-08-10 00:25:21 +0200

    Fix quadratic behaviour with alloc/free
    TODO: check fro memory leaks

--- src/mem/allocator.rs
+++ src/mem/allocator.rs
@@ -3,6 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+use std::collections::HashMap;
 use super::{GuestUSize, Mem, VAddr};
 use std::num::NonZeroU32;
 
@@ -22,6 +23,10 @@ impl Chunk {
             base,
             size: NonZeroU32::new(size).unwrap(),
         }
+    }
+
+    pub fn size(&self) -> GuestUSize {
+        self.size.get()
     }
 
     fn last_byte(&self) -> VAddr {
@@ -90,8 +95,9 @@ mod chunk_tests {
 /// Tracks which memory is in use and (TODO:) makes allocations from it.
 #[derive(Debug)]
 pub struct Allocator {
-    used_chunks: Vec<Chunk>,
     unused_chunks: Vec<Chunk>,
+
+    heap_used_chunks: HashMap<VAddr, Chunk>,
 }
 
 impl Allocator {
@@ -103,10 +109,13 @@ impl Allocator {
             Mem::NULL_PAGE_SIZE,
             Mem::MAIN_THREAD_STACK_LOW_END - Mem::NULL_PAGE_SIZE,
         );
+        let mut used_chunks = HashMap::new();
+        used_chunks.insert(null_page.base, null_page);
+        used_chunks.insert(main_thread_stack.base, main_thread_stack);
 
         Allocator {
-            used_chunks: vec![null_page, main_thread_stack],
             unused_chunks: vec![rest],
+            heap_used_chunks: used_chunks,
         }
     }
 
@@ -121,7 +130,7 @@ impl Allocator {
                     self.unused_chunks.push(after);
                 }
 
-                self.used_chunks.push(chunk);
+                self.heap_used_chunks.insert(chunk.base, chunk);
                 return;
             }
         }
@@ -132,15 +141,7 @@ impl Allocator {
     pub fn alloc(&mut self, size: GuestUSize) -> VAddr {
         // TODO: use a better allocation strategy, probably using buckets.
 
-        // iPhone OS's allocator always aligns to 16 bytes at minimum, and this
-        // is also the minimum allocation size.
-        // TODO: also do the 4096-byte alignment.
-        let size = size.max(16);
-        let size = if size % 16 != 0 {
-            size + 16 - (size % 16)
-        } else {
-            size
-        };
+        let size = Self::align_size(size);
 
         let existing_chunk = {
             let mut perfect_chunk: Option<usize> = None;
@@ -173,54 +174,76 @@ impl Allocator {
             }
         };
 
+        self.split_chunk(size, existing_chunk)
+    }
+
+    pub fn split_chunk(&mut self, size: GuestUSize, existing_chunk: Chunk) -> VAddr {
         if size < existing_chunk.size.get() {
             let alloc = Chunk::new(existing_chunk.base, size);
             let rump = Chunk::new(existing_chunk.base + size, existing_chunk.size.get() - size);
 
             let res = alloc.base;
-            self.used_chunks.push(alloc);
             self.unused_chunks.push(rump);
+            self.heap_used_chunks.insert(res, alloc);
             res
         } else {
-            assert!(size == existing_chunk.size.get());
+            assert_eq!(size, existing_chunk.size.get());
 
             let res = existing_chunk.base;
-            self.used_chunks.push(existing_chunk);
+            self.heap_used_chunks.insert(res, existing_chunk);
             res
         }
     }
 
+    pub fn align_size(size: GuestUSize) -> GuestUSize {
+        // iPhone OS's allocator always aligns to 16 bytes at minimum, and this
+        // is also the minimum allocation size.
+        // TODO: also do the 4096-byte alignment.
+        let size = size.max(16);
+        if size % 16 != 0 {
+            size + 16 - (size % 16)
+        } else {
+            size
+        }
+    }
+
     /// This is used for realloc
-    pub fn find_allocated_size(&mut self, base: VAddr) -> GuestUSize {
-        let Some(idx) = self.used_chunks.iter().position(|chunk| chunk.base == base) else {
+    pub fn find_allocated_chunk(&mut self, base: VAddr) -> Chunk {
+        let Some(&chunk) = self.heap_used_chunks.get(&base) else {
             panic!("Can't find {:#x}, unknown allocation!", base);
         };
-        let chunk = self.used_chunks.get(idx).unwrap();
-        chunk.size.get()
+        chunk
     }
 
     /// Returns the size of the freed chunk so it can be zeroed if desired
     #[must_use]
     pub fn free(&mut self, base: VAddr) -> GuestUSize {
-        let Some(idx) = self.used_chunks.iter().position(|chunk| chunk.base == base) else {
-            panic!("Can't free {:#x}, unknown allocation!", base);
+        let Some(&chunk) = self.heap_used_chunks.get(&base) else {
+            panic!("Can't find {:#x}, unknown allocation!", base);
         };
-        let chunk = self.used_chunks.remove(idx);
         let size = chunk.size.get();
 
+        self.heap_used_chunks.remove(&base).unwrap();
+
+        let (combined_chunk, _) = self.try_combine_with_neighbour(chunk, true);
+        self.unused_chunks.push(combined_chunk);
+
+        size
+    }
+
+    pub fn try_combine_with_neighbour(&mut self, chunk: Chunk, allow_left_grow: bool) -> (Chunk, bool) {
         if let Some(other_chunk_idx) = self.unused_chunks.iter().position(|other_chunk| {
             (other_chunk.base as u64) == (chunk.last_byte() as u64 + 1)
-                || (chunk.base as u64) == (other_chunk.last_byte() as u64 + 1)
+                || (allow_left_grow && (chunk.base as u64) == (other_chunk.last_byte() as u64 + 1))
         }) {
             let other_chunk = self.unused_chunks.swap_remove(other_chunk_idx);
             let combined = Chunk::new(
                 chunk.base.min(other_chunk.base),
                 chunk.size.get() + other_chunk.size.get(),
             );
-            self.unused_chunks.push(combined);
+            (combined, true)
         } else {
-            self.unused_chunks.push(chunk);
+            (chunk, false)
         }
-        size
     }
 }
